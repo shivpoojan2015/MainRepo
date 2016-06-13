d02484f12e047057b82b38e98e1ebac3373505ca
Poojan Patel <shiv.poojan.patel@accenture.com>
2016-06-13 12:35:32 +0530

    adding src folder from batch file

---
+++ git_commands.bat
@@ -0,0 +1,10 @@
+cd C:\DevOps\Jenkins25Demo\MainRepo
+set PATH=%PATH%;"C:\Program Files\Git\cmd"
+git checkout master
+git status
+git add .
+git commit -m "adding src folder from batch file"
+git remote add cloudOps https://shivpoojan2015:shivpoojan@123/shivpoojan2015/MainRepo.git
+git push -u cloudOps master
+
+pause
